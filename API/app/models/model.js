fe15ab4ce1b59f4af9899ac09060c79c111cbc62
ajjollymore <80071436+ajjollymore@users.noreply.github.com>
2022-02-25 19:21:41 -0500

    Initial Commit

---
+++ API/app/models/model.js
@@ -0,0 +1,118 @@
+/*
+defines commands sent to the sql database from controller.js
+*/
+const sql = require("./db.js");
+
+// constructor
+const User = function(user) {
+  this.name = user.name;
+  this.password = user.password;
+};
+
+User.create = (newUser, result) => {
+  sql.query("INSERT INTO users SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created user: ", { id: res.insertId, ...newUser });
+    result(null, { id: res.insertId, ...newUser });
+  });
+};
+
+User.findById = (id, result) => {
+  sql.query(`SELECT * FROM users WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found user: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found User with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+User.getAll = (name, result) => {
+  let query = "SELECT * FROM users";
+
+  if (name) {
+    query += ` WHERE name LIKE '%${name}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("users: ", res);
+    result(null, res);
+  });
+};
+
+User.updateById = (id, user, result) => {
+  sql.query(
+    "UPDATE users SET name = ?, password = ?, WHERE id = ?",
+    [user.name, user.password, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found User with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated user: ", { id: id, ...user });
+      result(null, { id: id, ...user });
+    }
+  );
+};
+
+User.remove = (id, result) => {
+  sql.query("DELETE FROM users WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found User with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted user with id: ", id);
+    result(null, res);
+  });
+};
+
+User.removeAll = result => {
+  sql.query("DELETE FROM users", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} users`);
+    result(null, res);
+  });
+};
+
+module.exports = User;
---
+++ API/app/routes/routes.js
@@ -0,0 +1,28 @@
+/*
+  when a request is made it gets routed to a different function in controller.js
+*/
+module.exports = app => {
+  const users = require("../controllers/controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new User
+  router.post("/users", users.create);
+
+  // Retrieve all Users
+  router.get("/users", users.findAll);
+
+  // Retrieve a single User with id
+  router.get("/users/:id", users.findOne);
+
+  // Update a User with id
+  router.put("/users/:id", users.update);
+
+  // Delete a User with id
+  router.delete("/users/:id", users.delete);
+
+  // Delete all Users
+  router.delete("/users", users.deleteAll);
+
+  app.use('/api', router);
+};
