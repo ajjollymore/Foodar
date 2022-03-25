e27530d982031453a8d89ac91b98295dd7ecd7bf
Harsh Kalyani <65092789+hkaly@users.noreply.github.com>
2022-03-25 12:46:16 -0400

    Added Order Confirmation Screen

---
+++ App/screens/customerConfirmationScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <View style = {styles.home}>
+        <Image source = {require('./img/home.png')}></Image></View>
+
+      <View style = {styles.confirmed}>
+        <Image source = {require('./img/checkmark.png')}></Image></View>
+
+      <Text style = {styles.title}>Thank you for your order!</Text></View>
+  )
+}
+
+const styles = StyleSheet.create({
+  home: {
+    marginTop: 20,
+    marginLeft: 20
+  },
+
+  confirmed: {
+    alignItems: 'center',
+    marginTop: 200,
+  },
+
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    padding: 50
+  },
+
+});
+
+export default App;
